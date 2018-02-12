435d39a7b304ad599ca60cb5aecd912ab19226d2
Mike Stankiewicz <stankiem89@gmail.com>
2018-02-11 22:44:09 -0700

    added content to files and got rid of useless CSS & HTML docs

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '7CC0g9dKzBsmKWMIiv0vr4qNI',
+  consumer_secret: 'c6RnXDinqceVi469Yn5Wab0zqnrxdW3c8oy0tihULmixVFlD4s',
+  access_token_key: '957464451283787776-QLnAtPwhYl73nhBn29FfykRCyY1a2xi',
+  access_token_secret: '957464451283787776-QLnAtPwhYl73nhBn29FfykRCyY1a2xi',
+}
+
+module.exports = twitterKeys;
